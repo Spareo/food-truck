0cbbace1ffd0bb67d7b92cdbbd71fb1053982355
Dmitry Khenkin <dkhenkin@gmail.com>
2020-06-13 13:01:32 -0500

    Added swagger UI dist so host swagger UI from github pages

--- src/FoodTruck.Web/Startup.cs
+++ src/FoodTruck.Web/Startup.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.Json.Serialization;
 using FoodTruck.Core.Interfaces;
@@ -11,6 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Serilog;
 using TinyCsvParser;
 
@@ -31,6 +36,19 @@ namespace FoodTruck.Web
             services.AddDbContext<FoodTruckContext>(opt => opt.UseInMemoryDatabase("FoodTruckDb"));
             services.AddScoped<IFoodTruckProvider, FoodTruckProvider>();
 
+            // Add Swagger UI
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Food Trucks Service API", Version = "v1" });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+
+
             services.AddHealthChecks()
                     .AddCheck("FoodTruckService", () => HealthCheckResult.Healthy("Servie is healthy!"));
 
@@ -47,6 +65,16 @@ namespace FoodTruck.Web
             // Initialize the DB with dat from the food truck CSV
             //var context = app.ApplicationServices.GetService<FoodTruckDbContext>();
             InitData(context);
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Food Trucks Service API");
+            });
 
             app.UseSerilogRequestLogging();
 
